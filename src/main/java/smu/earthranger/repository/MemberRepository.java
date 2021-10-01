1f3db7dab02776396cceab91a5963982209dad0b
younghyun-na <younghyunna@naver.com>
2021-10-01 15:14:09 +0900

    fix: repository

--- src/main/java/smu/earthranger/repository/MemberRepository.java
+++ src/main/java/smu/earthranger/repository/MemberRepository.java
@@ -1,8 +1,18 @@
 package smu.earthranger.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import smu.earthranger.domain.Member;
 
+import java.util.Optional;
+
+
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-        Member findMemberByEmail(String email);
+
+        Optional<Member> findMemberByEmail(String email);
+        Optional<Member> findUserByName(String name);
+
+        Page<Member> findAll(Pageable pageable);
 }
